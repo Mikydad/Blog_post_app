3c6f9b39a040843778c2d79dbe333fd8a98ab22a
Milkesa Dereje <69218039+Mikydad@users.noreply.github.com>
2025-11-16 15:58:57 +0300

    Fixed some bags and addes some styles to the blog detail page

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,51 +1,48 @@
-import React, { createContext, useContext, useState } from "react";
-
+import { Children, createContext, use, useContext, useState } from "react";
 type Post = {
-  id: string;
-  title: string;
-  description: string;
-  createdAt: string;
+    id: String;
+    title: String;
+    description: String;
+    createdAt:String;
 };
-
 type CreatePostPayload = {
-  title: string;
-  description: string;
-};
-
+    title: String;
+    description:String;
+}
 type AppContextValue = {
-  posts: Post[];
-  createPost: (payload: CreatePostPayload) => string;
+    posts: Post[],
+    createPost: (payload: CreatePostPayload) => String;
 };
 
 const AppContext = createContext<AppContextValue | null>(null);
 
-export const AppProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const [posts, setPosts] = useState<Post[]>([]);
+export const AppProvider: React.FC<{children: React.ReactNode}> = ({children}) => {
 
-  const createPost = ({ title, description }: CreatePostPayload): string => {
-    const addPost: Post = {
-      id: crypto.randomUUID(),
-      title,
-      description,
-      createdAt: new Date().toISOString(),
+    const [posts, setPosts] = useState<Post[]>([]);
+    const createPost =({title,description}:CreatePostPayload):String => {
+        const addPost: Post = {
+            id: crypto.randomUUID(),
+            title,
+            description,
+            createdAt: new Date().toISOString(),
+        }
+
+        setPosts((prev) => [addPost, ...prev]);
+
+        return addPost.id;
     };
 
-    setPosts((prev) => [addPost, ...prev]);
-
-    return addPost.id;
-  };
-
-  return (
-    <AppContext.Provider value={{ posts, createPost }}>
-      {children}
-    </AppContext.Provider>
-  );
-};
+    return(
+        <AppContext.Provider value = {{posts, createPost}}>
+        {children}
+        </AppContext.Provider>
+    )
+}
 
 export const useAppContext = () => {
-  const ctx = useContext(AppContext);
-  if (!ctx) {
-    throw new Error("useAppContext must be used within an AppProvider");
-  }
-  return ctx;
-};
+    const ctx = useContext(AppContext);
+
+    if(!ctx){
+        throw new Error("useAppContext must be used within app")
+    } return ctx;
+}
